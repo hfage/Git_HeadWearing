ba0355259b099d2071f8b72acd0fabf70fc7d07c
hfage <zhengfa.h@gmail.com>
2015-04-18 22:35:41 +0800

    test sync2
    test sync2

--- src/com/example/headwearing/MyLog.java
+++ src/com/example/headwearing/MyLog.java
@@ -6,7 +6,7 @@ public final class MyLog{
 	public static boolean DEBUG = true;
 	public static void i(String tag, String msg){
 		if(DEBUG)
-			Log.i(tag,msg);
+			Log.i(tag,msg); 
 	}
 	public static void d(String tag, String msg){
 		if(DEBUG)
